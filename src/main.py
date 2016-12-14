d2a66dacee08b93f05ec9e3a88f1c175bbf06231
fstrub <florian.strub@gmail.com>
2016-12-14 16:09:40 +0100

    First commit to test

---
+++ src/main.py
@@ -0,0 +1,10 @@
+import argparse
+
+
+if __name__ == '__main__':
+
+
+    parser = argparse.ArgumentParser(description='Argument to train Batch Policy Iteration Network.')
+
+    params = parser.parse_args()
+
